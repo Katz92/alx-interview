d0d62ef6b9f9712b2e5dcd75974dbe9c62a39a0f
root <katlegomagombo@gmail.com>
2024-05-01 04:35:09 -0700

    txt

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
+"""Change making module.
+"""
+
 
 def makeChange(coins, total):
-    # Initialize dp array with total+1 as we can't have more coins than total+1
-    dp = [total + 1] * (total + 1)
-    
-    # If total is 0, then answer is 0
-    dp[0] = 0
-    
-    # For each coin, we update our dp array
-    for coin in coins:
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
-    
-    # If dp[total] is still total+1, then we return -1 as no combination can sum to total
-    return dp[total] if dp[total] != total + 1 else -1
-
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
